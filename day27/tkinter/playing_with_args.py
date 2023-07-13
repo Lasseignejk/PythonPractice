12e231426336b7f5a4b2369506b33381fd11aec5
Lasseignejk <jaye914@hotmail.com>
2023-07-13 09:58:08 -0400

    args and kwargs

---
+++ day27/tkinter/playing_with_args.py
@@ -0,0 +1,14 @@
+def add(*args):
+    total = 0
+    for num in args:
+        total += num
+    print(total) 
+
+add(1, 2, 3, 4)
+
+def calculate(n, **kwargs):
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+    print(n)
+
+calculate(2, add=3, multiply=5)
