8ed52c9894ecee19382bc82b742eaac039f687fe
Lasseignejk <jaye914@hotmail.com>
2023-09-19 13:54:38 -0400

    Added exception handling to nato_alphabet

--- day26/nato_alphabet/main.py
+++ day26/nato_alphabet/main.py
@@ -1,22 +1,19 @@
 import pandas
 
-#TODO 1. Create a dictionary in this format:
-# {"A": "Alfa", "B": "Bravo"}
-
-data = pandas.read_csv("nato_phonetic_alphabet.csv")
+data = pandas.read_csv("day26/nato_alphabet/nato_phonetic_alphabet.csv")
 
 alphabet_dict = {row.letter:row.code for (index, row) in data.iterrows()}
 # print(alphabet_dict)
 
-
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-
 isOn = True 
 while isOn: 
-    user_input = input("What would you like to translate? ")
+    user_input = input("What would you like to translate? ").upper()
     if user_input == "q" or user_input == "Q":
         isOn = False
         print("Thank you for using this service!")
         break
-    answer = [alphabet_dict[letter] for letter in user_input.upper() if letter in alphabet_dict]
-    print(answer)
+    try:
+        answer = [alphabet_dict[letter] for letter in user_input]
+        print(answer)
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
