d3f897dee86f39c2081cd8bd2490c8e1ec153cdd
lasseignejk <jaye914@hotmail.com>
2025-02-27 13:52:11 -0500

    simple weather app

--- dave_grey_tutorials/venv/weather.py
+++ dave_grey_tutorials/venv/weather.py
@@ -15,11 +15,11 @@ def get_current_weather():
 
 	weather_data = requests.get(request_url).json()
 
-	# pprint(weather_data)
+	pprint(weather_data)
 
-	print(f"\nCurrent weather for {weather_data["name"]}")
-	print(f"\nThe temp is {weather_data["main"]['temp']}")
-	print(f"\nFeels like {weather_data["main"]['feels_like']} and {weather_data['weather'][0]['description']}.")
+	print(f"\nCurrent weather for {weather_data['name']}")
+	print(f"\nThe temp is {weather_data['main']['temp']}")
+	print(f"\nFeels like {weather_data['main']['feels_like']} and {weather_data['weather'][0]['description']}.")
 
 if __name__ == "__main__":
 	get_current_weather()
